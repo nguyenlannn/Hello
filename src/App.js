20576d83f281b80055e7556d1dd63d69d5efa9d2
nguyenlannn <lannhatthuy@gmail.com>
2022-09-23 21:12:26 +0700

    lan

---
+++ src/App.js
@@ -0,0 +1,35 @@
+
+import './App.css';
+
+function App() {
+  return (<>
+    <div className='row'>
+      <div className='col-md-4 col-sm-6'>
+        <div className='p-3 mb-3 bg-primary text-white'>primary</div>
+      </div>
+      <div className='col-md-4 col-md-4 col-sm-6'>
+        <div className='p-3 mb-3 bg-secondary text-white'>secondary</div>
+      </div>
+      <div className='col-md-4 col-md-4 col-sm-6'>
+        <div className='p-3 mb-3 bg-success text-white'>success</div>
+      </div>
+      <div className='col-md-4 col-md-4 col-sm-6'>
+        <div className='p-3 mb-3 bg-danger text-white'>danger</div>
+      </div>
+      <div className='col-md-4 col-md-4 col-sm-6'>
+        <div className='p-3 mb-3 bg-warning text-dark'>warning</div>
+      </div>
+      <div className='col-md-4 col-md-4 col-sm-6'>
+        <div className='p-3 mb-3 bg-info text-dark'>info</div>
+      </div>
+      <div className='col-md-4 col-md-4 col-sm-6'>
+        <div className='p-3 mb-3 bg-light text-dark'>light</div>
+      </div>
+      <div className='col-md-4 col-md-4 col-sm-6'>
+        <div className='p-3 mb-3 bg-dark text-white'>dark</div>
+      </div>
+    </div>
+  </>);
+}
+
+export default App;
